82b3ef21b25e41c828c9d0581029d2a98293e2cb
emmanuel <oemmanuel.uwadiae@gmail.com>
2024-09-15 10:39:00 +0100

    adding create user endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, { Request, Response } from "express"
-import cors from "cors"
-import "dotenv/config"
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoute";
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to db :)"));
 
-app.get("/test", async (req: Request, res: Response) => {
-    res.json({message: "Yo!"});
-})
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/my/user", myUserRoute);
 
 app.listen(7000, () => {
-    console.log("server started on localhost:7000")
-})
+  console.log("server started on localhost:7000");
+});
